3f71b14765db17c5c67d7dc71d933f528f8ae2d0
Gowtham Kumar <108609884+GowthamKumarMandala@users.noreply.github.com>
2023-03-07 19:29:05 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# myself.in
-This is my personal portfolio website to know about my skills and capability.
+# myself.co
---
+++ script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // scroll arrow
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    })
+    
+
+        // TYPING 
+    var typed = new Typed(".typing",{
+        strings :["Student","Developer","Editor","Dancer"],
+        backSpeed:60,
+        typeSpeed:100,
+        loop:true
+    });
+    var typed = new Typed(".typing2",{
+        strings :["Student","Developer","Editor","Dancer"],
+        backSpeed:60,
+        typeSpeed:100,
+        loop:true
+    });
+
+
+
+    // toggle manu/navbar scriopt
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    
+
+    // OWL CAROUSEL SCRIPT
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
+user = prompt("kindly enter your name");
+document.getElementById('user').innerHTML=`<H1>HELLO ${user} WELCOME TO THIS WEBSITE</H1>`;
+  
+let uname = document.getElementById("usub");
+let uemail = document.getElementById("uemail");
+let usub = document.getElementById("usub");
+let udesc = document.getElementById("desc");
+var body = 'name:'+uname+"<br/>" +'Email:'+ uemail+"<br/>" +'subject:'+usub+"<br/>" +'Desc:'+ udesc;
+let join = "some one joined your portfolio";
+
+function sendEmail(){
+    
+    alert("your details submitted thanks for visiting, please give feedback by clicking 'sendfeedback'")
+}
+
