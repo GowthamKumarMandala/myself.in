3bc16c10eb94b4a0cfc939834291ea193f1e239e
Gowtham Kumar <108609884+GowthamKumarMandala@users.noreply.github.com>
2023-07-23 22:07:31 +0530

    Update script.js FOR welcome msg username

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ $(document).ready(function(){
     });
 });
 user = prompt("kindly enter your name");
-if(user==null)`<H1>HELLO  WELCOME TO THIS WEBSITE</H1>`;
-document.getElementById('user').innerHTML=`<H3>HELLO ${user} WELCOME TO THIS WEBSITE</H3>`;
+if(user==null)`<H1>HELLO  WELCOME TO MY WEBSITE</H1>`;
+document.getElementById('user').innerHTML=`<H3>HELLO ${user} WELCOME TO MY WEBSITE</H3>`;
   
 let uname = document.getElementById("usub");
 let uemail = document.getElementById("uemail");
